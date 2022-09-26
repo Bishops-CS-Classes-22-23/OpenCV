4b6f00dc28d62e3006ef2f05a631cb9cb7cdd476
Rilky1 <nathan.xiong.24@bishops.com>
2022-09-26 15:04:59 -0700

    Formatting and cleaning code up

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # OpenCV
- OpenCV Project: Blurring the Background of a Subject
+ OpenCV Project: Adding a Virtual Background To a Subject
+
 
 Chapters:
 
-Chapter 3 - Loading and Displaying Images
+Chapter 3 - Loading and Displaying Images, Getting Image Info
+Functions: resizeBackground(), calling in images in main()
+
+Chapter 4 - Manipulating Pixels
+Functions: combineImages()
+
 Chapter 6 - Cropping Image, Masking
-Chapter 8 - Gaussian Blurring (?)
+Functions: processImage()
+
 Chapter 10 - Canny 
+Functions: processImage()
+
 Chapter 11 - Contours
+Functions: processImage()
+
 Dilate
+Functions: processImage()
+
+Variation on Masking: combining images by looping through each pixel
+Functions: combineImages()
--- blurface.py
+++ blurface.py
@@ -1,21 +1,6 @@
 import numpy as np
-import argparse
 import cv2
 
-
-def getImage():
-    """
-    Purpose: Reads in the given image in the command line
-    Parameters: None
-    Returns: Returns the processed image
-    (Chapter 3)
-    """
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required = True,
-    help = "Path to the image")
-    args = vars(ap.parse_args())
-    image = cv2.imread(args["image"])
-    return image
 
 def processImage(image, threshold1, threshold2):
     """
@@ -25,29 +10,27 @@ def processImage(image, threshold1, threshold2):
     Returns: Cropped image
     (Chapters 6, 10, 11, & dilate)
     """
-    # man: 10, 60
-    # boy: 100, 200
-    # rice: 100, 200
-    # hw: 20, 175
+  
     canned = cv2.Canny(image, threshold1, threshold2)
     # cv2.imshow("Canny", canned)
 
     # apply mask (thick outline)
     kernel = np.ones((5,5),np.uint8)
     mask = cv2.dilate(canned, kernel, iterations = 1)
-    # cv2.imshow("Mask real", mask)
+    cv2.imshow("Mask real", mask)
 
     # find contours
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-    crop_mask = np.zeros_like(mask) 
-    cv2.drawContours(crop_mask, contours, -1, (255), -1)
-    # cv2.imshow("crop mask", crop_mask) 
+    # crop_mask = np.zeros_like(mask) 
+    cv2.drawContours(mask, contours, -1, (255), -1)
+    cv2.imshow("crop mask", mask) 
     
     # crop image
     crop = np.zeros_like(image) 
-    crop[crop_mask == 255] =  image[crop_mask == 255]
+    crop[mask == 255] = image[mask == 255]
 
     return crop
+
 
 def resizeBackground(imgName, crop):
     """
@@ -60,10 +43,10 @@ def resizeBackground(imgName, crop):
     background = cv2.imread(imgName)
     # crop pic to fit image size
     backHeight, backWidth, backChannels = background.shape
-    print("background:", backHeight, backWidth)
+    print("background dimensions:", backHeight, backWidth)
 
     cropHeight, cropWidth, cropChannels = crop.shape
-    print("image:", cropHeight, cropWidth)
+    print("image dimensions:", cropHeight, cropWidth)
 
     cropBack = cv2.resize(background, (cropWidth, cropHeight))
     resizedCrop = cv2.resize(crop, (cropWidth, cropHeight))
@@ -73,37 +56,6 @@ def resizeBackground(imgName, crop):
 
     return cropBack, resizedCrop
 
-def blurImgBackground(image, crop):
-    """
-    Purpose:
-    Parameters:
-    Returns:
-    """
-
-    blurred = cv2.blur(image, (15, 15))
-    # cv2.imshow("original", image)
-    # cv2.imshow("blurred", blurred)
-
-    finalImage = crop.copy()
-    finalHeight, finalWidth, finalChannels = finalImage.shape
-    print(finalHeight, finalWidth)
-    for x in range(finalHeight):
-        for y in range(finalWidth):
-            # print(x,y)
-            b, g, r = finalImage[x,y]
-            # print(b, g, r)
-
-            # check if pixel in crop img is on/off
-            # if off: get pixel at exact location in background and draw onto black pixel
-            # if on: don't change
-            if b == 0 and g == 0 and r == 0:
-                # get pixel values of background
-                backPixel = blurred[x,y]
-                finalImage[x][y] = np.array(backPixel)
-   
-    return finalImage
-
-
 
 def combineImages(resizedCrop, cropBack):
     """
@@ -111,11 +63,11 @@ def combineImages(resizedCrop, cropBack):
     drawing the background onto the black (off) pixels of the cropped image  
     Parameters: Cropped image and same dimension background
     Returns: Returns the finished image with the cropped image over the background
-    (Combining images by looping through each pixel; variation on masking)
+    (Chapter 4 & Combining images by looping through each pixel; variation on masking)
     """
     finalImage = resizedCrop.copy()
     finalHeight, finalWidth, finalChannels = finalImage.shape
-    print(finalHeight, finalWidth)
+    # print(finalHeight, finalWidth)
     for x in range(finalHeight):
         for y in range(finalWidth):
             # print(x,y)
@@ -133,7 +85,6 @@ def combineImages(resizedCrop, cropBack):
     return finalImage
 
 
-
 def main():
 
     image = cv2.imread("boy.jpeg")
@@ -143,9 +94,10 @@ def main():
     # hw: 20, 175
     # beach: 600, 150
     crop = processImage(image, 100, 200)
-    blurFinal = blurImgBackground(image, crop)
-    cropBack, resizedCrop = resizeBackground("beachback.jpg", crop)
+    # blurFinal = blurImgBackground(image, crop)
+    cropBack, resizedCrop = resizeBackground("beachback.jpeg", crop)
     boyBackFinal = combineImages(resizedCrop, cropBack)
+    cv2.imshow("Boy Original", image)
     cv2.imshow("Boy Background Final", boyBackFinal)
     cv2.waitKey(0)
 
@@ -156,23 +108,39 @@ def main():
     # hw: 20, 175
     # beach: 600, 150
     crop = processImage(image2, 100, 200)
-    blurFinal = blurImgBackground(image2, crop)
-    cropBack, resizedCrop = resizeBackground("beachback.jpg", crop)
+    # blurFinal = blurImgBackground(image2, crop)
+    cropBack, resizedCrop = resizeBackground("beachback.jpeg", crop)
     riceBackFinal = combineImages(resizedCrop, cropBack)
+    cv2.imshow("Rice Original", image2)
     cv2.imshow("Rice Background Final", riceBackFinal)
     cv2.waitKey(0)
 
-    image3 = cv2.imread("hw.webp")
+    image3 = cv2.imread("hw.jpeg")
     # man: 10, 60
     # boy: 100, 200
     # rice: 100, 200
     # hw: 20, 175
     # beach: 600, 150
     crop = processImage(image3, 20, 175)
-    blurFinal = blurImgBackground(image3, crop)
-    cropBack, resizedCrop = resizeBackground("beachback.jpg", crop)
+    # blurFinal = blurImgBackground(image3, crop)
+    cropBack, resizedCrop = resizeBackground("beachback.jpeg", crop)
     hwBackFinal = combineImages(resizedCrop, cropBack)
+    cv2.imshow("HW Original", image3)
     cv2.imshow("HW Background Final", hwBackFinal)
+    cv2.waitKey(0)
+
+    image4 = cv2.imread("beach.jpeg")
+    # man: 10, 60
+    # boy: 100, 200
+    # rice: 100, 200
+    # hw: 20, 175
+    # beach: 600, 150
+    crop = processImage(image4, 600, 150)
+    # blurFinal = blurImgBackground(image3, crop)
+    cropBack, resizedCrop = resizeBackground("beachback.jpeg", crop)
+    hwBackFinal = combineImages(resizedCrop, cropBack)
+    cv2.imshow("Beach Original", image4)
+    cv2.imshow("Beach Background Final", hwBackFinal)
     cv2.waitKey(0)
 
     # show 
@@ -181,20 +149,4 @@ def main():
     # cv2.imshow("Blurred Final", blurFinal)
 
 main()
-
-
-
-# cv2.imshow("cropBack", cropBack)
-# cv2.imshow("resizedCrop", resizedCrop)
-
-
-# cv2.imshow("Original", image) 
-# blurred = cv2.blur(image, (15, 15))
-# cv2.imshow("blurred", blurred)
-
-# masked = cv2.bitwise_xor(crop, cropBack)
-# cv2.imshow("original", image)
-# cv2.imshow("Final", masked)
-
-cv2.waitKey(0) 
 
